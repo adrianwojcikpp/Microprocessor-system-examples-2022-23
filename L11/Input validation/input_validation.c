c4e6523f7bd2d4805e5636786c05d2d7f9aec1ab
Adrian Wójcik <54798218+adrianwojcikpp@users.noreply.github.com>
2023-05-28 20:06:32 +0200

    L11 Examples

--- L11/Input validation/input_validation.c
+++ L11/Input validation/input_validation.c
@@ -1,15 +1,28 @@
-/******************************************************************************
+/**
+  ******************************************************************************
+  * @file    input_validation.c
+  * @author  AW		adrian.wojcik@put.poznan.pl
+  * @version 3.0
+  * @date    22-May-2022
+  * @brief   User input validation example
+  *
+  ******************************************************************************
+  */
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
+/* Includes ------------------------------------------------------------------*/
 #include <stdio.h>
 #include <string.h>
 
-_Bool MESSAGE_ContainsOnly(char* msg, char* key)
+/* Private function ----------------------------------------------------------*/
+/**
+ * @brief Checks whether message contains only given characters
+ * 
+ * @param[in] msg : User input
+ * @param[in] key : String with allowed characters
+ * @retval 1 if user input contains only characters in given key
+ *         0 otherwise
+ */
+_Bool MESSAGE_ContainsOnly(const char* msg, const char* key)
 {
     while(*msg != '\0')
     {
@@ -20,22 +33,47 @@ _Bool MESSAGE_ContainsOnly(char* msg, char* key)
     return 1;
 }
 
-_Bool MESSAGE_TerminateWith(char* msg, char* terminator)
+/**
+ * @brief Checks whether message is terminated with given sequence
+ * 
+ * @param[in] msg : User input
+ * @param[in] terminator : Expected sequence at the and of user input
+ * @retval 1 if user input is terminated as expected
+ *         0 otherwise
+ */
+_Bool MESSAGE_TerminateWith(const char* msg, const char* terminator)
 {
     int p = strlen(msg) - strlen(terminator);
     return (&msg[p] == strstr(msg,terminator));
 }
 
-_Bool MESSAGE_IsValid(char* msg)
+/**
+ * @brief Checks whether message is valid: contains only '0', '1',
+ *        '\r' and '\n' and is terminated with "\r\n" sequence
+ * 
+ * @param[in] msg : User input
+ * @retval 1 if user input is valid
+ *         0 otherwise
+ */
+_Bool MESSAGE_IsValid(const char* msg)
 {
     return MESSAGE_ContainsOnly(msg, "01\r\n") && MESSAGE_TerminateWith(msg, "\r\n");
 }
 
+/* Main function -------------------------------------------------------------*/
+
+/**
+  * @brief  The application entry point.
+  * @retval 0 
+  */
 int main()
 {
-    char msg[] = "101010101\r\n";
-    
-    printf("%s: %u\n", msg, MESSAGE_IsValid(msg));
+    // User input
+    char msg[] = "101010101\r\n"; // valid input 
+    //char msg[] = "101010101\n\r"; // invalid input 
+
+    // Print validation results
+    printf("User input content: %s\nUser input is %svalid\n", msg, MESSAGE_IsValid(msg) ? "" : "in");
 
     return 0;
 }
