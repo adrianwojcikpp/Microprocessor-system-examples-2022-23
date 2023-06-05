470e75598e075da7a91f4ef64e1881898d52d858
Adrian Wójcik <54798218+adrianwojcikpp@users.noreply.github.com>
2023-06-05 19:36:55 +0200

    Final Task Example
    npn to pnp

--- FT/code/Firmware template/main.c
+++ FT/code/Firmware template/main.c
@@ -1,19 +1,19 @@
 /**
   ******************************************************************************
-  * @file    main.c
-  * @author  AW           Adrian.Wojcik@put.poznan.pl
+  * @file     main.c
+  * @author   AW           Adrian.Wojcik@put.poznan.pl
   * @author
   * @author
-  * @version 2.0
-  * @date	 02-Jun-2022
-  * @brief	 Main file: simple test display_control library
+  * @version  2.0
+  * @date     02-Jun-2022
+  * @brief    Main file: simple test display_control library
   *
   ******************************************************************************
   */
 
 /* Includes ------------------------------------------------------------------*/
-#include "hal_gpio.h" 				//< HAL GPIO mock
-#include "display_control.h"    	//< DISPLAY CONTROL LIBRARY
+#include "hal_gpio.h"               //< HAL GPIO mock
+#include "display_control.h"        //< DISPLAY CONTROL LIBRARY
 #include "display_control_config.h" //< DISPLAY CONTROL CONFIGURATION SETTINGS
 
 /* Main function -------------------------------------------------------------*/
